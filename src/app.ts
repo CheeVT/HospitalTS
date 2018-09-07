49f92e5e648da10863f044492c405d3e838cd194
Jovan Nanic <jovannanic@MacBook-Pro-6.local>
2018-09-07 13:06:28 +0200

    break

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,23 @@ import {factory} from "./class/ConfigLog4j";
 import Doctor from './class/Doctor';
 import Patient from './class/Patient';
 import {LaboratoryExamination, BloodPressure, BloodSugarLevel} from './class/LaboratoryExamination';
-//import BloodPressure from './class/BloodPressure';
-//import BloodSugarLevel from './class/BloodSugarLevel';
 
 const logDoctor = factory.getLogger("model.Doctor");
 
 window.onload = function() {
+    //create a doctor
     let milan = new Doctor('Milan', 'Milic', 'Dental');
+    //create a patient
     let dragan = new Patient('Dragan', 'Draganovic', '0705990847542', '092');
-
+    //patient choose a doctor
     dragan.chooseDoctor(milan);
 
-    //logger.debug("Casting debug magic spell: " + milan.firstName);
-    //logDoctor.info('Doctor created: ' + milan.firstName + ' ' + milan.lastName);
+    
+    //doctor create appointments and assign to a patient
+    let appointment1 = new BloodPressure(new Date("September 14, 2018 10:00:00"), '', '', '');
+    milan.makeAppointment(dragan, appointment1);
 
-    //alert('dsaads');
+    let appointment2 = new BloodSugarLevel(new Date("September 14, 2018 10:15:00"), '', '');
+    milan.makeAppointment(dragan, appointment2);
 
-    let pregled1 = new BloodPressure(new Date("February 4, 2016 10:13:00"), '', '', '');
-
-    milan.makeAppointment(dragan, pregled1);
 }
